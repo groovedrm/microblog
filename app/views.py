b68b5a021dfa644321abf4952e051e7645cb5961
ccocuzzo <ccocuzzo@nephilaadvisors.com>
2017-01-27 19:24:20 -0500

    Second commit adding in the template language

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index/')
 def index ():
-	return "Hello, this is my app!"
+	user = {'nickname': 'Chris'}
+	posts = [
+		{
+			'author': {'nickname': 'John'},
+			'body': 'Beautiful day in New York!'
+		},
+		{
+			'author': {'nickname': 'Chris'},
+			'body': 'Beautiful day for Chris!'
+		}
+	]
+	
+	return render_template('index.html', title='Home', user=user, posts=posts)
